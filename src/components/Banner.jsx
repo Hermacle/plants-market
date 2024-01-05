15395b882a4088c7b85dac7d88b99be016a9878b
Hermacle <kihenyeghohermacle@gmail.com>
2024-01-05 13:42:32 +0100

    Feat: Banner css

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,6 +1,7 @@
+import '../styles/Banner.css'
 function Banner(){
   return (
-    <h1>La maison de la jungle</h1>
+    <h1 className="lmj-banner">La maison de la jungle</h1>
   )
 } 
 export default Banner;
