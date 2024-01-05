3920c2dd20d4b1304f8a0ae60329a4de0aa94a2a
Hermacle <kihenyeghohermacle@gmail.com>
2024-01-05 13:59:26 +0100

    Feat: add logo

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,7 +1,12 @@
+import logo from '../assets/logo.png';
 import '../styles/Banner.css'
 function Banner(){
+  const title ='La maison de la jungle'
   return (
-    <h1 className="lmj-banner">La maison de la jungle</h1>
+    <div className="lmj-banner">
+      <img src={logo} alt="La maison de la jungle" className="lmj-logo" />
+      <h1 className="lmj-title">{title}</h1>
+    </div>
   )
 } 
 export default Banner;
