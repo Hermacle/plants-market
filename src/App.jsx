066460fb81fee72590fa0f5066ae216f7e196fd4
Hermacle <kihenyeghohermacle@gmail.com>
2024-01-05 15:32:22 +0100

    Feat : Shopping items

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Banner from './components/Banner'
 import Card from './components/Card'
+import ShoppingList from './components/ShoppingList'
 
 function App() {   
   return (
     <>
     <Banner />
     <Card />
+    <ShoppingList />
     </>
   )
 }
